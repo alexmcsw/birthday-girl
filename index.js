a3c0e7ab618006cdabd17eb353e0d58862cb67b8
Alex McSween <alexmcsw@gmail.com>
2024-05-17 16:14:22 -0400

    make the colour scheme match my invitations :3

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function GetValue()
 {
-    var myarray= new Array("Lord of the Rings if the Hobbits slayed","Upsidedown world","aliens or whatever");
+    var myarray= new Array(
+        "aliens or whatever",
+        "any universe from Everything Everywhere All at Once",
+        "2010s YA dystopia",
+        "a past life",
+        "you in 50 years",
+        "you 20 years ago",
+        "the last video game/movie/book you played/watched/read",
+        "Lord of the Rings if the hobbits slayed"
+
+
+    );
     var random = myarray[Math.floor(Math.random() * myarray.length)];
    document.getElementById("message").innerHTML=random;
 }
