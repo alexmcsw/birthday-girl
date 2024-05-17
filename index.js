d928b0fb14b287e5b4f3adcfd94db4844d564b6e
Alex McSween <alexmcsw@gmail.com>
2024-05-16 22:12:31 -0400

    getting closer!

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
 function GetValue()
 {
-    var myarray= new Array("hi","bye","hello");
+    var myarray= new Array("Lord of the Rings if the Hobbits slayed","Upsidedown world","aliens or whatever");
     var random = myarray[Math.floor(Math.random() * myarray.length)];
    document.getElementById("message").innerHTML=random;
 }
 
-
-const confettiAmount = 100;
-let btnClick = document.querySelector('button1');
-let btnHover = document.querySelector('.hoverme');
-
-btnClick.onclick = () => throwConfetti(btnClick);
-btnHover.onmousemove = () => throwConfetti(btnHover);
-
-function throwConfetti(btn) {
-  
-  let df = document.createDocumentFragment();
-  for (let i=0; i<confettiAmount; i++) {
-    let c = document.createElement("i");
-    c.style.cssText = `
-      transform: translate3d(${(random(500) - 250)}px, ${(random(225) - 150)}px, 0)
-      rotate(${ random(360) }deg); 
-      background: hsla(${random(360)},100%,50%,1);
-    `;
-    df.appendChild(c);
-    setTimeout(()=> {
-      c.remove();
-    }, 2000)  
-  } 
-  btn.appendChild(df);
-
-}
-
-function random(max){
-  return Math.random() * (max - 0) + 0;
-}
